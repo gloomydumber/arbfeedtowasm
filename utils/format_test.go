09e469f34cd63796c1a667c53fad436d158eb002
gloomydumber <chromacrow@naver.com>
2024-10-20 17:51:18 +0900

    `refactor` : indentation

--- utils/format_test.go
+++ utils/format_test.go
@@ -11,38 +11,38 @@ import (
 )
 
 func TestParseIncomingMessage(t *testing.T) {
-    cases := []struct {
-        name          string
-        feedMessage   string
-        expected      feedtypes.IncomingMessage
-    }{
-        {
-            name:        "JavaScript Object Case",
-            feedMessage: test.ExampleFeedMessageJSObject,
-            expected:    test.ExampleParsedMessage,
-        },
-        {
-            name:        "JSON Case",
-            feedMessage: test.ExampleFeedMessageJSON,
-            expected:    test.ExampleParsedMessage,
-        },
-        {
-            name:        "Stringified Case",
-            feedMessage: test.ExampleFeedMessageString,
-            expected:    test.ExampleParsedMessage,
-        },
-    }
+	cases := []struct {
+		name        string
+		feedMessage string
+		expected    feedtypes.IncomingMessage
+	}{
+		{
+			name:        "JavaScript Object Case",
+			feedMessage: test.ExampleFeedMessageJSObject,
+			expected:    test.ExampleParsedMessage,
+		},
+		{
+			name:        "JSON Case",
+			feedMessage: test.ExampleFeedMessageJSON,
+			expected:    test.ExampleParsedMessage,
+		},
+		{
+			name:        "Stringified Case",
+			feedMessage: test.ExampleFeedMessageString,
+			expected:    test.ExampleParsedMessage,
+		},
+	}
 
-    for _, tc := range cases {
-        t.Run(tc.name, func(t *testing.T) {
-            feedMessage := tc.feedMessage
-            if utils.IsJSObject(feedMessage) {
-                feedMessage = utils.ConvertToJSON(feedMessage)
-            }
-            parsedMessage := utils.ParseIncomingMessage(feedMessage)
-            if diff := cmp.Diff(tc.expected, parsedMessage); diff != "" {
-                t.Errorf("Test %s failed (-expected +got):\n%s", tc.name, diff)
-            }
-        })
-    }
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			feedMessage := tc.feedMessage
+			if utils.IsJSObject(feedMessage) {
+				feedMessage = utils.ConvertToJSON(feedMessage)
+			}
+			parsedMessage := utils.ParseIncomingMessage(feedMessage)
+			if diff := cmp.Diff(tc.expected, parsedMessage); diff != "" {
+				t.Errorf("Test %s failed (-expected +got):\n%s", tc.name, diff)
+			}
+		})
+	}
 }
